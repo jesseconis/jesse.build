d6d0d685728d819840fa0043662ea5d25fe12e74
Jesse <dev@jesseconis.io>
2025-07-14 18:51:54 +1000

    chore:💤 update tasks,posts pages

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,7 +3,7 @@ import { title } from "@/components/primitives";
 export default function TasksPage() {
   return (
     <div>
-      <h1 className={title()}>Tasks</h1>
+      <h1 className={title()}>Posts</h1>
     </div>
   );
 }
