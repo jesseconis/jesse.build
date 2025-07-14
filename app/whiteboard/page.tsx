c654519a67071b3a9622a589ab8e31deaec3d65b
Jesse <dev@jesseconis.io>
2025-07-14 18:53:38 +1000

    feat:✨ add whiteboard/excalidraw boilerplate

---
+++ app/whiteboard/page.tsx
@@ -0,0 +1,10 @@
+import { title } from "@/components/primitives";
+
+export default function WhiteboardPage() {
+  return (
+    <div>
+      <h1 className={title()}>Whiteboard</h1>
+    </div>
+  );
+}
+
